fe67957a4498dd8a25c56691cd4243599ac70c1e
Sebastian Tomaszkiewicz <sebastianmtomaszkiewicz@gmail>
2020-03-17 11:18:04 +0100

    Updates

---
+++ precache-manifest.b2ee0eb670a6a9b745110b6a4fbba188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b3c6b21668a554839f7711392b9b6c",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "9fe60bf6286b86b0252d",
+    "url": "/To-do-list/static/css/main.96bcee29.chunk.css"
+  },
+  {
+    "revision": "2adb46c58703b83f0eed",
+    "url": "/To-do-list/static/js/2.de65926b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/To-do-list/static/js/2.de65926b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fe60bf6286b86b0252d",
+    "url": "/To-do-list/static/js/main.e562689d.chunk.js"
+  },
+  {
+    "revision": "338181695ae4e898358b",
+    "url": "/To-do-list/static/js/runtime-main.238bfe3d.js"
+  }
+]);
